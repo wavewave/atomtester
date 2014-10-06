9e24cc83a1dbdd5abae4b83d89a2699a74c7219c
Ian-Woo Kim <ianwookim@gmail.com>
2014-10-06 22:15:28 +0000

    now generated lhe file can be fed into PYTHIA8

--- lhegun.hs
+++ lhegun.hs
@@ -14,21 +14,21 @@ tevinfo = EvInfo { nup = 4
                  , aqcdup = 0.1135437
                  } 
 
-tptlinfo_ep = PtlInfo { ptlid = 1
+tptlinfo_ep (x,y,z) = PtlInfo { ptlid = 1
                       , idup = -11
                       , istup = -1
                       , mothup = (0,0)
                       , icolup = (0,0)
-                      , pup = (0, 0, 7000, 7000,0)
+                      , pup = (0, 0, energy (x,y,z), energy (x,y,z), 0)
                       , vtimup = 0
                       , spinup = 1 } 
 
-tptlinfo_em = PtlInfo { ptlid = 2
+tptlinfo_em (x,y,z) = PtlInfo { ptlid = 2
                       , idup = 11
                       , istup = -1
                       , mothup = (0,0)
                       , icolup = (0,0)
-                      , pup = (0, 0, -7000, 7000,0)
+                      , pup = (0, 0, - (energy (x,y,z)), energy (x,y,z), 0 )
                       , vtimup = 0
                       , spinup = -1 } 
 
@@ -37,18 +37,18 @@ energy (x,y,z) = sqrt (x*x + y*y + z*z)
 
 tptlinfo_u (x,y,z) = PtlInfo { ptlid = 3
                              , idup = 2
-                             , istup = -1
-                             , mothup = (0,0)
-                             , icolup = (0,0)
+                             , istup = 1
+                             , mothup = (1,2)
+                             , icolup = (501,0)
                              , pup = (x, y, z, energy (x,y,z), 0)
                              , vtimup = 0
                              , spinup = -1 } 
 
 tptlinfo_ubar (x,y,z) = PtlInfo { ptlid = 4
                                 , idup = -2
-                                , istup = -1
-                                , mothup = (0,0)
-                                , icolup = (0,0)
+                                , istup = 1
+                                , mothup = (1,2)
+                                , icolup = (0,501)
                                 , pup = (x, y, z, energy (x,y,z), 0)
                                 , vtimup = 0
                                 , spinup = 1 } 
@@ -66,7 +66,7 @@ go gen pt eta = do
     return (px,py,pz)  
 
 lheXML :: String -> String
-lheXML str = "<LesHouchesEvents version=\"3.0\">\n<init>\n      -11       11  0.70000000000E+04  0.70000000000E+04 0 0       0       0 3   1\n  0.88308027210E-03  0.72579022363E-06  0.88308000000E-07   0\n<generator name='MadGraph5_aMC@NLO' version='#5.2.1.2'>please cite 1405.0301 </generator>\n</init>\n"
+lheXML str = "<LesHouchesEvents version=\"1.0\">\n<init>\n      -11       11  0.70000000000E+04  0.70000000000E+04 0 0       0       0 3   1\n    5.220106E+00  5.384128E-01  1.000000E+00    81\n  2.602564E-01  1.062492E-01  1.000000E+00    82\n</init>\n"
               ++ str ++ "</LesHouchesEvents>\n"
 
 eventXML :: String -> String
@@ -82,11 +82,13 @@ genEvent evgen gen pt eta = do
 
 
 testlhe :: (Double,Double,Double) -> LHEvent
-testlhe (x,y,z) = LHEvent tevinfo [tptlinfo_ep, tptlinfo_em, tptlinfo_u (x,y,z), tptlinfo_ubar (-x,-y,-z)]
+testlhe (x,y,z) = LHEvent tevinfo [ tptlinfo_ep (x,y,z)
+                                  , tptlinfo_em (x,y,z)
+                                  , tptlinfo_u (x,y,z)
+                                  , tptlinfo_ubar (-x,-y,-z)]
 
 main :: IO ()
 main = do
-  putStrLn "standard candle lhe"
   gen <- newMTGen Nothing
   str <- genEvent testlhe gen 100 1.0
   putStrLn (lheXML str)
